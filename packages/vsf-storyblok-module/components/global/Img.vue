f372c6a4f6e53b250a9698f11e1ffe41cc559556
Dmitrijs Sitovs <dmitrijs@magecroco.com>
2025-04-10 11:58:41 +0300

    - Minor fixes for the Img component

--- packages/vsf-storyblok-module/components/global/Img.vue
+++ packages/vsf-storyblok-module/components/global/Img.vue
@@ -39,7 +39,7 @@ export default {
       }
     },
     placeholderSrc () {
-      return this.placeholder || `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${this.intrinsicWidth} ${this.intrinsicHeight}"%3E%3C/svg%3E`
+      return this.placeholder || `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${this.intrinsicWidth || this.width } ${this.intrinsicHeight || this.height }"%3E%3C/svg%3E`
     },
     isSvg () {
       return this.src.endsWith('.svg')
